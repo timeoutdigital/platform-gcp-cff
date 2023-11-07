5ebb70b870bd61640b609c9f919f84db04db7cbf
Keith Harvey <keith.harvey@timeout.com>
2023-11-07 17:10:33 +0000

    BQ viewer and jobuser on lnd for data engineers (#6)

--- blueprints/data-solutions/data-platform-minimal/01-landing.tf
+++ blueprints/data-solutions/data-platform-minimal/01-landing.tf
@@ -21,10 +21,12 @@ locals {
     "roles/storage.objectAdmin"   = [module.processing-sa-0.iam_email]
     "roles/bigquery.dataEditor"   = [module.processing-sa-0.iam_email]
     "roles/bigquery.dataViewer" = [
+      local.groups_iam.data-engineers,
       local.dataform_sa_iam_email,
       local.looker_sa_iam_email
     ]
    "roles/bigquery.jobUser" = [
+     local.groups_iam.data-engineers,
      local.looker_sa_iam_email,
      module.processing-sa-0.iam_email
    ]
