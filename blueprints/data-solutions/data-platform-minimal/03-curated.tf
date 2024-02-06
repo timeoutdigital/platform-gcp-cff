53b1d67b4cdd54d30af403163674689f60cb7b11
Keith Harvey <keith.harvey@timeout.com>
2024-02-06 11:24:43 +0000

    linty

--- blueprints/data-solutions/data-platform-minimal/03-curated.tf
+++ blueprints/data-solutions/data-platform-minimal/03-curated.tf
@@ -16,7 +16,7 @@
 
 locals {
   cur_iam = {
-    "roles/bigquery.dataOwner"  = [module.processing-sa-0.iam_email]
+    "roles/bigquery.dataOwner" = [module.processing-sa-0.iam_email]
     "roles/bigquery.dataViewer" = [
       module.cur-sa-0.iam_email,
       local.groups_iam.data-analysts,
